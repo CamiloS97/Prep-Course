8cfb4a25e1c39d1b572dbd36834b1fcbf9ab66cf
CamiloS97 <camilosisci@gmail.comm>
2022-06-15 20:48:23 -0300

    aver

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -80,6 +80,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num<90) {
+    return true;
+  } else {
+    return false;
+  }
   
 }
 
@@ -87,12 +92,16 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num>50) {
+    return true;
+  } else {return false;}
   
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  return x%y;
   
 }
 
@@ -100,6 +109,9 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num%2==0){
+    return true;
+  }else {return false;}
   
 }
 
@@ -107,6 +119,9 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num%2==1){
+    return true;
+  }else {return false;}
   
 }
 
