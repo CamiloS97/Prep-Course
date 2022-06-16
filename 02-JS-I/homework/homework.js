0aff24fc8d1608af9da2da0f678ef379c36af37e
CamiloS97 <camilosisci@gmail.comm>
2022-06-15 21:36:18 -0300

    final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -167,7 +167,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  Math.random();
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -175,9 +175,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero>=0){
+  if (numero>0){
     return "Es positivo";
-  }else {return "Es negativo"}
+  }else {return "Es negativo";}
 }
 
 function agregarSimboloExclamacion(str) {
@@ -201,7 +201,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var saludo = "hola" + nombre + "!"
+  var saludo = "Hola" + nombre + "!"
   return saludo;  
 }
 
