e2b1e9374d666c2c5551753d81523936da105c65
CamiloS97 <camilosisci@gmail.comm>
2022-06-15 23:54:51 -0300

    prueba3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -128,16 +128,16 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 
-  if (num1>num2&&num1>num3&&num1>0){
-    return "Número 1 es mayor y positivo"
-  } else if (num1<0 || num2<0 || num3<0){
+  if (num1<0 || num2<0 || num3<0){
     return "Hay negativos";
-  }else if (num3>num1&&num3>num2){
+  }else if (num1===0 || num2===0 || num3===0){
+    return "Error";
+  } else if (num1>num2&&num1>num3){
+    return "Número 1 es mayor y positivo"
+  } else  if (num3>num1&&num3>num2){
     num3 ++;
     return num3;
-  } else if (num1===0 || num2===0 || num3===0){
-    return "Error";
-  } else {return false;}
+  } else  {return false;}
 
 }
 
@@ -147,6 +147,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero !==1 && numero !==0){
+    for (1=2; i<numero; i++){
+      if(numero%1===0){
+        return false;
+      }return true;
+    }
+  } return false;
 }
 
 function esVerdadero(valor){
@@ -154,18 +161,35 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
+  switch (valor){
+    case true:
+      return "Soy verdadero";
+    
+    case false:
+      return "Soy falso";
+  }
+
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+  var tabla = [];
+
+  for (i=0; i<=10; i++){
+    tabla.push(6*i)
+    return "6 * " + i + "= " + tabla[i]; 
+  }
   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if(numero>99&&numero<1000){
+    return true;
+  }else {return false;}
   
 }
 
@@ -173,7 +197,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+
+  do { 
+    i = i +5;
+  }
+   while(i<=40);
 }
+
 
 
 // No modificar nada debajo de esta línea
